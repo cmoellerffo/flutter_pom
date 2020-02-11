4486c96a6738b665d797df897718e654a7fca399
cmoeller <christian.moeller@vivasecur.de>
2020-02-11 12:14:03 +0100

    ADD: More in-line Documentation. ADD: Copyright Header to Source Files

--- lib/model/database.dart
+++ lib/model/database.dart
@@ -1,13 +1,72 @@
+/*
+BSD 2-Clause License
+
+Copyright (c) 2020, VIVASECUR GmbH
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import 'dart:io';
 
 import 'package:flutter_pom/context/base_model_context.dart';
 import 'package:flutter_pom/context/migration_context.dart';
 import 'package:flutter_pom/context/model_context.dart';
-import 'package:flutter_pom/context/pom_logger.dart';
 import 'package:flutter_pom/model/migration_info.dart';
 import 'package:flutter_pom/model/table.dart';
 import 'package:sqflite/sqflite.dart' as b;
 
+/// Represents a physical SQLite Database containing [Table]s that are
+/// built based on code-first column definition
+///
+/// Based on whether you want the provided [Database] to automatically
+/// Migrate your [Table]s based on Revision numbers the [Database] will
+/// try to add columns introduced in later revisions by creating the
+/// corresponding '''ALTER TABLE''' statements.
+///
+/// - **DO** create a new [Database] model definition
+///
+/// '''dart
+/// class MyDatabaseModel extends Database {
+///
+///   // Call super and provide the database name
+///   MyDatabaseModel() : super('my_db.db');
+///
+///   // Return all tables that you want your database to contain
+///   @override
+///   Map<Type, Table> initializeDatabase() {
+///     return <Type, Table> {
+///       MySampleTable: MySampleTable()
+///     };
+///   }
+/// }
+/// '''
+///
+/// - **DON'T** return a variable list inside **initializeDatabase()** that
+///   can change over time.
+///
+/// - **DON'T** use the **dbHandle** property to make modifications to your
+///   tables as the [Database] will not track changes made directly.
+///
 abstract class Database {
   Map<Type, Table> _tables;
   Map<Type, ModelContext> _modelTables = <Type, ModelContext>{};
@@ -28,6 +87,7 @@ abstract class Database {
   /// Gets or sets whether the connection gets opened automatically
   bool autoOpen = true;
 
+  /// Gets or sets whether to do auto migration
   bool enableMigration = false;
 
   /// Creates a new Instance
@@ -40,15 +100,21 @@ abstract class Database {
     }
   }
 
+  /// Runs the automatic migration of all [Table]s inside this [Database] by
+  /// checking the automatically stored [$MigrationInfo] with the current
+  /// [Table] revision.
+  ///
+  /// When there is no [$MigrationInfo] available for a [Table], a new
+  /// migration will be executed for the selected [Table]
+  ///
+  /// - **KEEP IN MIND** that we currently only support **Adding Columns**
+  ///
   Future<void> _doMigrations() async {
     _migrationContext = ModelContext<$MigrationInfo>(this, _migrationInfo);
     await _migrationContext.create();
 
     _tables.forEach((t, v) async {
       var table = _tables[t];
-
-      //PomLogger.instance.log.d("*** Running Migration for '${table.tableName}' ***");
-
       var tableInfo =
         await _migrationContext.select((q) {
           return q.where(_migrationContext.fields.name.equals(table.tableName));
